185b526ab95bfb3e15df5a3b36921b593aaaceee
Pierrick <deshayes.pierrick@hotmail.fr>
2022-05-01 19:59:33 +0200

    ReactJS Tutorial - 15 - Methods as props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Component } from 'react';
 import './App.css';
-import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EventBind />
+        <ParentComponent />
+        {/* <EventBind /> */}
         {/* <FunctionClick /> */}
         {/* <ClassClick /> */}
         {/* <Counter /> */}
