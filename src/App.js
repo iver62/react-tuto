1c60ce92f2529f85fad3485cfc900411069de9ac
Pierrick <deshayes.pierrick@hotmail.fr>
2022-05-01 19:22:13 +0200

    ReactJS Tutorial - 12 - Destructuring props and state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { Component } from 'react';
 import './App.css';
-import Counter from './components/Counter';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        {/* <Counter /> */}
         {/* <Message /> */}
         {/* <Greet name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman" >
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wonder Woman" />
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" /> */}
+          <p>This is children props</p>
+        </Greet>
+        <Greet name="Clark" heroName="Superman" >
+          <button>Action</button>
+        </Greet> */}
+        <Greet name="Diana" heroName="Wonder Woman" />
+        <Welcome name="Bruce" heroName="Batman" />
+        {/* <Welcome name="Clark" heroName="Superman" /> */}
+        {/* <Welcome name="Diana" heroName="Wonder Woman" /> */}
       </div>
     );
   }
