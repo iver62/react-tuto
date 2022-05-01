0ab04d4e0420b5c9a3d62568639d5ed0555c36ad
Pierrick <deshayes.pierrick@hotmail.fr>
2022-05-01 19:34:00 +0200

    ReactJS Tutorial - 13 - Event Handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
+import ClassClick from './components/ClassClick';
+import FunctionClick from './components/FunctionClick';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FunctionClick />
+        <ClassClick />
         {/* <Counter /> */}
         {/* <Message /> */}
         {/* <Greet name="Bruce" heroName="Batman">
@@ -15,8 +17,8 @@ class App extends Component {
         <Greet name="Clark" heroName="Superman" >
           <button>Action</button>
         </Greet> */}
-        <Greet name="Diana" heroName="Wonder Woman" />
-        <Welcome name="Bruce" heroName="Batman" />
+        {/* <Greet name="Diana" heroName="Wonder Woman" /> */}
+        {/* <Welcome name="Bruce" heroName="Batman" /> */}
         {/* <Welcome name="Clark" heroName="Superman" /> */}
         {/* <Welcome name="Diana" heroName="Wonder Woman" /> */}
       </div>
