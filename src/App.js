a1da5c1526ae22af9efc82b1381d7e481fafbbcd
Pierrick <deshayes.pierrick@hotmail.fr>
2022-06-19 10:54:23 +0200

    20 - Styling and CSS Basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Component } from 'react';
 import './App.css';
-import NameList from './components/NameList';
+import './appStyles.css';
+import styles from './appStyles.module.css';
+import Inline from './components/Inline';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NameList />
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
+        <Inline />
+        {/* <Stylesheet primary={true} /> */}
+        {/* <NameList /> */}
         {/* <UserGreeting /> */}
         {/* <ParentComponent /> */}
         {/* <EventBind /> */}
