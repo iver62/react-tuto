7d942cf3a255ecdafe16405a8a044600312b3665
Pierrick <deshayes.pierrick@hotmail.fr>
2022-07-24 11:58:26 +0200

    ReactJS Tutorial - 25 - Fragments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 import './appStyles.css';
-import LifecycleA from './components/LifecycleA';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <LifecycleA />
+        {/* <FragmentDemo /> */}
+        <Table />
       </div>
     );
   }
